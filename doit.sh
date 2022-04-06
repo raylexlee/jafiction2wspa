c01cbff36e87022305b41acc5acffb2fd3573404
Raylex Lee <raylexym57lee@gmail.com>
2022-04-06 19:53:09 +0800

    Fine tune workflow

---
+++ doit.sh
@@ -0,0 +1,8 @@
+./jws 坊っちゃん https://ja.wikisource.org/wiki/坊つちやん
+./jws 倫敦塔 https://ja.wikisource.org/wiki/倫敦塔_\(小説\)
+./jws 三四郎 https://ja.wikisource.org/wiki/三四郎
+./jws こゝろ https://ja.wikisource.org/wiki/こゝろ
+./jws 硝子戸の中 https://ja.wikisource.org/wiki/硝子戸の中
+./jws 明暗 https://ja.wikisource.org/wiki/明暗
+./jws ウィキペディア日本語版 https://ja.wikipedia.org/wiki/メインページ
+
